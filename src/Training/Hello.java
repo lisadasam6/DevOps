3bb352310c465aa2bffaa0e441d0ab7d841ee56f
lisadasam6 <lisadasam6@gmail.com>
2022-03-16 18:28:03 -0400

    Checking on this project

--- src/Training/Hello.java
+++ src/Training/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is devops training session");
+		System.out.println("I did my first project,hooray");
 
 	}
 
